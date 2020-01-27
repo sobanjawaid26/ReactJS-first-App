de530e1512c20a08f284829249daabd2abf7ffc5
sobanjawaid <sobanjawaid@vinnovatelabz.com>
2020-01-27 13:21:58 +0530

    commit needed for using eject

--- src/App.js
+++ src/App.js
@@ -105,9 +105,9 @@ class App extends Component {
                 <div className="App">
                     <h1>Hi, I'm a React App</h1>
                     <p className={classes.join(' ')}>This is really working!</p>
-                    <StyledButton
-                        alt={this.state.showPersons}
-                        onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>
+                    <button
+                        className="button"
+                        onClick={this.togglePersonsHandler}>Toggle Persons</button>
                     {persons}
                 </div>
         );
