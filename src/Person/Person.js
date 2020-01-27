73850a7c83c6dc58d5cf0db0d08acafccfd0132b
sobanjawaid <sobanjawaid@vinnovatelabz.com>
2020-01-27 11:24:16 +0530

    radium used to add hover effect on button

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,8 +2,13 @@ import React from 'react';
 import './Person.css';
 
 const person = (props) => {
+    const style = {
+        '@media (min-width: 500px)': {
+            width: '450px'
+        }
+    }
     return (
-        <div className="Person">
+        <div className="Person" style={style}>
             <p onClick={props.click}>My name is {props.name}. I am {props.age} years old</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
